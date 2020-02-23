02ebe056b66b1d6c2df96283965f2d10cef192d0
Evelyn Cristini Oliveira <30937883+EvyOliveira@users.noreply.github.com>
2020-02-23 18:03:23 -0300

    "feat: validação de peso e altura com mensagem ao usuário"

--- js/principal.js
+++ js/principal.js
@@ -13,6 +13,22 @@ var altura = tdAltura.textContent;
 
 var tdImc = paciente.querySelector(".info-imc");
 
-var imc = peso / (altura * altura);
+var pesoEhValido = true;
+var alturaEhValido = true;
 
+if(peso <= 0 || peso >= 1000) {
+    console.log("Peso inválido");
+    pesoEhValido = false;
+    tdImc.textContent = "Peso Inválido";
+}
+
+if (altura <= 0 || altura >= 3.00) {
+    console.log("Altura inválida!");
+    alturaEhValido = false;
+    tdImc.textContent = "Altura Inválida!";
+}
+
+if (alturaEhValido && pesoEhValido) {
+var imc = peso / (altura * altura);
 tdImc.textContent = imc;
+}
